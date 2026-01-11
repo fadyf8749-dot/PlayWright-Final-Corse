d7515911366ba9078a57749fbb719a67f3a1bd5b
Fady <fadyf8749@gmail.com>
2026-01-11 17:22:38 +0200

    🐛 FIX: ..

--- tests/Loginstyle.test.ts
+++ tests/Loginstyle.test.ts
@@ -1,28 +1,29 @@
 import { chromium, expect, test } from "@playwright/test";
 
-const capabilities = {
-  browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-  browserVersion: "latest",
-  "LT:Options": {
-    platform: "Windows 10",
-    build: "Playwright Test Build",
-    name: "Playwright Test",
-    user: process.env.LT_USERNAME,
-    accessKey: process.env.LT_ACCESS_KEY,
-    network: true,
-    video: true,
-    console: true,
-    tunnel: false, // Add tunnel configuration if testing locally hosted webpage
-    tunnelName: "", // Optional
-    geoLocation: "", // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-  },
-};
+// const capabilities = {
+//   browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+//   browserVersion: "latest",
+//   "LT:Options": {
+//     platform: "Windows 10",
+//     build: "Playwright Test Build",
+//     name: "Playwright Test",
+//     user: process.env.LT_USERNAME,
+//     accessKey: process.env.LT_ACCESS_KEY,
+//     network: true,
+//     video: true,
+//     console: true,
+//     tunnel: false, // Add tunnel configuration if testing locally hosted webpage
+//     tunnelName: "", // Optional
+//     geoLocation: "", // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+//   },
+// };
 test("Login test demo", async () => {
-  let browser = await chromium.connect(
-    `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
-      JSON.stringify(capabilities)
-    )}`
-  );
+  let browser = await chromium.launch();
+  // connect(
+  //   `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+  //     JSON.stringify(capabilities)
+  //   )}`
+  // );
 
   let context = await browser.newContext();
   let page = await context.newPage();
